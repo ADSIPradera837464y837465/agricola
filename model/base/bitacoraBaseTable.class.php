04fc17fbbb568919450687e60b4573b0c2f8d66d
DIANA <DIANA@DIANA-PC.home>
2015-11-21 16:24:06 -0500

    mapeo tabla bitacora

---
+++ model/base/bitacoraBaseTable.class.php
@@ -0,0 +1,292 @@
+<?php
+
+namespace FStudio\model\base;
+
+use FStudio\fsModel as model;
+use FStudio\myConfig as config;
+
+/**
+ * Description of bitacoraBaseTable
+ * @author Diana Meneses <meneses_d@rocketmail.com>
+ * @package 
+ * @subpackage model
+ * @subpackage base
+ * @version 1.0.0
+ */
+class bitacoraBaseTable extends model {
+
+  /**
+   * id de la tabla
+   */
+  const ID = 'bit_id';
+
+  /**
+   * Foranea en usuario
+   */
+  const USUARIO_ID = 'usr_id';
+
+  /**
+   * Accion de bitácora
+   */
+  const ACCION = 'bit_accion';
+
+  /**
+   * Longitud del campo acción
+   */
+  const ACCION_LENGTH = 80;
+
+  /**
+   * nombre de la tabla donde se ejecuta la accion
+   */
+  const TABLA = 'bit_tabla';
+
+  /**
+   * Longitud de la bit_tabla
+   */
+  const TABLA_LENGTH = 80;
+
+  /**
+   * Numero de registro
+   */
+  const REGISTRO = 'bit_registro';
+
+  /**
+   * Observaciones que se realizan en la tabla   
+   */
+  const OBSERVACION = 'bit_observacion';
+
+  /**
+   * Longitud del observacion
+   */
+  const OBSERVACION_LENGTH = 1024;
+
+  /**
+   * Fecha de registro
+   */
+  const FECHA = 'bit_fecha';
+
+  /**
+   * Secuencia de la identificacion de la tabla
+   */
+  const _SEQUENCE = '';
+
+  /**
+   * Nombre de la tabla
+   */
+  const _TABLE = 'bda_bitacora';
+
+  /**
+   * Configuración del sistema
+   * @var config
+   */
+  protected $config;
+
+  /**
+   * Id de la tabla
+   * @var integer 
+   */
+  private $id;
+
+  /**
+   * Accion realizada
+   * @var string 
+   */
+  private $accion;
+
+  /**
+   * primaria de usuario
+   * @var integer 
+   */
+  private $usuarioId;
+
+  /**
+   * descripcion realizada
+   * @var varchar 
+   */
+  private $observacion;
+
+  /**
+   * nombre de la tabla
+   * @var varchar 
+   */
+  private $tabla;
+
+  /**
+   * Descripcion del registro
+   * @var varchar
+   */
+  private $registro;
+
+  /**
+   * fecha en la que se realiza el registro
+   * @var varchar
+   */
+  private $fecha;
+
+  /**
+   * 
+   * Constructor de la clase bitacoraBaseTable
+   * @param integer $id
+   * @param string $accion
+   * @param integer $usuarioId
+   * @param string $observacion
+   * @param string $tabla
+   * @param integer $registro
+   * @param date_time $fecha
+   */
+  public function __construct(config $config, $id = null, $accion = null, $usuarioId = null, $observacion = null, $tabla = null, $registro = null, $fecha = null) {
+    $this->config = $config;
+    $this->id = $id;
+    $this->accion = $accion;
+    $this->usuarioId = $usuarioId;
+    $this->observacion = $observacion;
+    $this->tabla = $tabla;
+    $this->registro = $registro;
+    $this->fecha = $fecha;
+  }
+
+  /**
+   * Retorna la configuración del sistema
+   * @version 1.0.0
+   * @return config
+   */
+  public function getConfig() {
+    return $this->config;
+  }
+
+  /**
+   * Retorna el id del registro
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * retorna la accion realizada
+   * @version 1.0.0
+   * @return varchar
+   */
+  public function getAccion() {
+    return $this->accion;
+  }
+
+  /**
+   * retorna el id (identificacion) del usuario
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getUsuarioId() {
+    return $this->usuarioId;
+  }
+
+  /**
+   * Retorna las observaciones realizadas
+   * @version 1.0.0
+   * @return varchar
+   */
+  public function getObservacion() {
+    return $this->observacion;
+  }
+
+  /**
+   * Retorna el nombre de la tabla
+   * @version 1.0.0
+   * @return varchar
+   */
+  public function getTabla() {
+    return $this->tabla;
+  }
+
+  /**
+   * REtorna numero del registro
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getRegistro() {
+    return $this->registro;
+  }
+
+  /**
+   * retorna fecha de registro
+   * @version 1.0.0
+   * @return date_time
+   */
+  public function getFecha() {
+    return $this->fecha;
+  }
+
+  /**
+   * Fija la configuración del sistema
+   * @version 1.0.0
+   * @param config $config Objeto con configuración del sistema
+   */
+  public function setConfig(config $config) {
+    $this->config = $config;
+  }
+
+  /**
+   * Fija el id para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $id
+   */
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  /**
+   * Fija la accion que se realiza (borrar,insertar)
+   * @version 1.0.0
+   * @param varchar $accion
+   */
+  public function setAccion($accion) {
+    $this->accion = $accion;
+  }
+
+  /**
+   * Fija el id del usuario
+   * @version 1.0.0
+   * @param integer $usuarioId
+   */
+  public function setUsuarioId($usuarioId) {
+    $this->usuarioId = $usuarioId;
+  }
+
+  /**
+   * fija las observaciones que se realizaron
+   * @version 1.0.0
+   * @param varchar $observacion
+   */
+  public function setObservacion($observacion) {
+    $this->observacion = $observacion;
+  }
+
+  /**
+   * Fija el nombre de la tabla
+   * @version 1.0.0
+   * @param varchar $tabla
+   */
+  public function setTabla($tabla) {
+    $this->tabla = $tabla;
+  }
+
+  /**
+   * fija registro realizado
+   * @version 1.0.0
+   * @param integer $registro
+   */
+  public function setRegistro($registro) {
+    $this->registro = $registro;
+  }
+
+  /**
+   * Fija La fecha de creacion del registro
+   * @version 1.0.0
+   * @param date $bit_fecha
+   */
+  public function setFecha($fecha) {
+    $this->fecha = $fecha;
+  }
+
+}
---
+++ model/bitacoraTable.class.php
@@ -0,0 +1,117 @@
+<?php
+
+use FStudio\model\base\bitacoraBaseTable;
+
+/**
+ * Description of bitacoraTable
+ * @author Diana Meneses <meneses_d@rocketmail.com>
+ * @package
+ * @subpackage model
+ * @subpackage table
+ * @version 1.0.0
+ */
+class bitacoraTable extends bitacoraBaseTable {
+
+  /**
+   * Obtiene todos los datos de la tabla
+   * @version 1.0.0
+   * @return [stdClass|boolean]
+   */
+  public function getAll() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'SELECT bit_id, usr_id, bit_accion, bit_tabla, bit_registro, bit_observacion, bit_fecha FROM bitacora ORDER BY bit_fecha ASC';
+    $answer = $conn->prepare($sql);
+    $answer->execute();
+    return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
+  }
+
+  /**
+   * Retorna un elemento de la tabla buscado por un ID especifico
+   * @version 1.0.0
+   * @param integer $id ID a buscar
+   * @return mixed [stdClass | boolean]
+   */
+  public function getById($id) {
+    $conn = $this->getConnection($this->config);
+    $sql = 'SELECT bit_id, usr_id, bit_accion, bit_tabla, bit_registro, bit_observacion, bit_fecha '
+            . 'FROM bda_bitacora '
+            . 'AND bit_id = :bit_id';
+    $params = array(
+        ':id' => $id
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
+  }
+
+  /**
+   * Registra los datos del objeto en la tabla
+   * @version 1.0.0
+   * @return integer
+   */
+  public function save() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'INSERT INTO bda_bitacora (bit_accion, usr_id, bit_observacion, bit_tabla, bit_registro,bit_fecha) VALUES (:bit_accion, :usr_id, :bit_observacion, :bit_tabla, :bit_registro, :bit_fecha)';
+    $params = array(
+        ':bit_accion' => $this->getAccion(),
+        ':usr_id' => $this->getUsuarioId(),
+        ':bit_observacion' => $this->getObservacion(),
+        ':bit_tabla' => $this->getTabla(),
+        ':bit_registro' => $this->getRegistro(),
+        ':bit_fecha' => $this->getFecha()
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return $conn->lastInsertId(self::_SEQUENCE);
+  }
+
+  /**
+   * Actualiza o modifica un registro de la tabla
+   * @version 1.0.0
+   * @return boolean
+   */
+  public function update() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'UPDATE bda_bitacora SET bit_accion = :bit_accion, usr_id = :usr_id, bit_observacion = :bit_observacion, bit_tabla = :bit_tabla, bit_registro = :bit_registro, bit_fecha = :bit_fecha WHERE bit_id = :bit_id';
+    $params = array(
+        ':bit_accion' => $this->getAccion(),
+        ':usr_id' => $this->getUsuarioId(),
+        ':bit_observacion' => $this->getObservacion(),
+        ':bit_tabla' => $this->getTabla(),
+        ':bit_registro' => $this->getRegistro(),
+        ':bit_id' => $this->getId(),
+        ':bit_fecha' => $this->getFecha()
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return true;
+  }
+
+  /**
+   * Borra en forma logica o fisica un registro de la tabla
+   * @version 1.0.0
+   * @param boolean $deleteLogical
+   * @return boolean
+   * @throws PDOException
+   */
+  public function delete($deleteLogical = true) {
+    $conn = $this->getConnection($this->config);
+    $params = array(
+        ':bit_id' => $this->getId()
+    );
+    switch ($deleteLogical) {
+      case true:
+        $sql = 'UPDATE bda_bitacora SET deleted_at = now() WHERE bit_id = :bit_id';
+        break;
+      case false:
+        $sql = 'DELETE FROM bda_bitacora WHERE bit_id = :bit_id';
+        break;
+      default:
+        throw new PDOException('Por favor indique un dato coherente para el borrado lógico o físico');
+    }
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return true;
+  }
+
+}
