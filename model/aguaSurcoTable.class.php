d5abcdb09ad4f9b6c53e98f313d9d7d3707d31c4
Usuario <Usuario@Equipo.home>
2015-11-23 18:09:43 -0500

    Mapeo de la tabla AguaSurco

---
+++ model/aguaSurcoTable.class.php
@@ -0,0 +1,106 @@
+<?php
+
+use FStudio\model\base\aguaSurcoBaseTable;
+/**
+ * Description of aguaSurcoTable
+ * @author Johanna G <ladyjkaulitz@hotmail.com>
+ * @package
+ * @subpackage model
+ * @subpackage table
+ * @version 1.0.0
+ */
+class aguaSurcoTable extends aguaSurcoBaseTable { 
+  
+  /**
+   * Obtiene todos los datos de la tabla
+   * @version 1.0.0
+   * @return [stdClass|boolean]
+   */
+  
+  public function getAll() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'SELECT deaas_id, deaas_item, deaas_cantidad_surco, fore_num_documento,created_at, updated_at,  deleted_at FROM aguaSurco ORDER BY created_at ASC';
+    $answer = $conn->prepare($sql);
+    $answer->execute();
+    return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
+}
+
+  /**
+   * Retorna un elemento de la tabla buscado por un ID especifico
+   * @version 1.0.0
+   * @param integer $id 
+   * @return mixed [stdClass | boolean]
+   */
+
+ public function getById($id) {
+    $conn = $this->getConnection($this->config);
+    $sql = 'SELECT deaas_id, deaas_item, deaas_cantidad_surco, fore_num_documento,created_at, updated_at,  deleted_at '
+            . 'FROM aguaSurco '
+            . 'AND deaas_id = :deaas_id';
+    $params = array(
+        ':deaas_id' => $id
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
+  }
+
+   /**
+   * Registra los datos del objeto en la tabla
+   * @version 1.0.0
+   * @return integer
+   */
+
+  public function save() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'INSERT INTO aguaSurco (deaas_item, deaas_cantidad_surco,fore_num_documento) VALUES (:deaas_item, :deaas_cantidad_surco, :fore_num_documento)';
+    $params = array(
+        ':deaas_item' => $this->getItem(),
+        ':deaas_cantidad_surco' => $this->getCantidadSurco(),
+        ':fore_num_documento' => $this->getNumDocumento(),
+       
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return $conn->lastInsertId(self::_SEQUENCE);
+  }
+  
+  /**
+   * Actualiza o modifica un registro de la tabla
+   * @version 1.0.0
+   * @return boolean
+   */
+public function update() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'UPDATE aguaSurco SET deaas_item = :deaas_item, deaas_cantidad_surco = :deaas_cantidad_surco, fore_num_documento = :fore_num_documento WHERE deaas_id = :deaas_id';
+    $params = array(
+     ':deaas_item' => $this->getItem(),
+        ':deaas_cantidad_surco' => $this->getCantidadSurco(),
+        ':fore_num_documento' => $this->getNumDocumento(),
+        ':deaas_id' => $this->getId()
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return true;
+  }
+  
+  
+  /**
+   * Borra en forma logica o fisica un registro de la tabla
+   * @version 1.0.0
+   * @param boolean $deleteLogical
+   * @return boolean
+   * @throws PDOException
+   */
+  
+    public function delete() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'DELETE FROM aguaSurco WHERE deaas_id = :deaas_id';
+    $params = array(
+        ':deaas_id' => $this->getId()
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return true;
+  }
+  }
---
+++ model/base/aguaSurcoBaseTable.class.php
@@ -0,0 +1,266 @@
+<?php
+
+namespace FStudio\model\base;
+
+use FStudio\fsModel as model;
+use FStudio\myConfig as config;
+
+
+/**
+ * Description of aguaSurcoBaseTable
+ * @author Johanna G <ladyjkaulitz@hotmail.com>
+ * @package
+ * @subpackage model
+ * @subpackage table
+ * @version 1.0.0
+ */
+class aguaSurcoBaseTable extends model {
+
+  /**
+   * ID de la tabla
+   */
+  const ID = 'deaas_id';
+
+  /**
+   * Secuencia del identificador de la tabla
+   */
+  const ITEM = 'deaas_item';
+  
+  /**
+   * Longitud del campo item 
+   */
+  const ITEM_LENGTH = 80;
+
+  /**
+   * Cantidad del surco
+   * 
+   */
+  const CANTIDAD_SURCO = 'deaas_cantidad_surco';
+
+  /**
+   * Numero del documento
+   */
+  const NUM_DOCUMENTO = 'fore_num_documento';
+
+  /**
+   * Secuencia de la tabla
+   */
+  const _SEQUENCE = '';
+
+  /**
+   * Tabla 
+   */
+  const _TABLE = 'bda_aguaSurco';
+
+  /**
+   * Configuración del sistema
+   * @var config
+   */
+  protected $config;
+
+  /**
+   * ID de la tabla
+   * @var integer
+   */
+  private $id;
+
+  /**
+   * Item de la tabla
+   * @var string
+   */
+  private $item;
+
+  /**
+   * Cantidad del surco 
+   * @var integer
+   */
+  private $cantidadSurco;
+
+  /**
+   * Numero del documento 
+   * @var integer
+   */
+  private $numDocumento;
+
+  /**
+   * Fecha y hora de la creacion de un nuevo registro 
+   * 
+   * @var date_time
+   */
+  private $createdAt;
+
+  /**
+   * Fecha y hora de la  actualizacion de un registro 
+   * date_time
+   * @var 
+   */
+  private $updatedAt;
+
+  /**
+   * Fecha y hora de la eliminacion de un registro 
+   * date_time
+   * @var 
+   */
+  private $deletedAt;
+
+  /**
+   * Constructor de la clase aguaSurcoBaseTable
+   * @version 1.0.0
+   * @param config $config
+   * @param integer $id
+   * @param integer $item
+   * @param integer $cantidadSurco
+   * @param integer $numDocumento
+   * @param date_time $createdAt
+   * @param date_time $updatedAt
+   * @param date_time $deletedAt
+   */
+  public function __construct(config $config, $id = null, $item = null, $cantidadSurco = null, $numDocumento = null, $createdAt = null, $updatedAt = null, $deletedAt = null) {
+    $this->config = $config;
+    $this->id = $id;
+    $this->item = $item;
+    $this->cantidadSurco = $cantidadSurco;
+    $this->numDocumento = $numDocumento;
+    $this->createdAt = $createdAt;
+    $this->updatedAt = $updatedAt;
+    $this->deletedAt = $deletedAt;
+  }
+
+  /**
+   * Retorna la configuración del sistema
+   * @version 1.0.0
+   * @return config
+   */
+  public function getConfig() {
+    return $this->config;
+  }
+
+  /**
+   * Retorna el id del registro
+   * @return integer
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * Retorna el item del registro
+   * @return integer
+   */
+  public function getItem() {
+    return $this->item;
+  }
+
+  /**
+   * Retorna la cantidad de registros
+   * @return integer
+   */
+  public function getCantidadSurco() {
+    return $this->cantidadSurco;
+  }
+
+  /**
+   * Retorna el numero del registro
+   * @return integer
+   */
+  public function getNumDocumento() {
+    return $this->numDocumento;
+  }
+
+  /**
+   * Retorna un registro creado
+   * @return integer
+   */
+  public function getCreatedAt() {
+    return $this->createdAt;
+  }
+
+  /**
+   * Retorna la actualizacion de un registro
+   * @return integer
+   */
+  public function getUpdatedAt() {
+    return $this->updatedAt;
+  }
+
+  /**
+   * Retorna la eliminacion de un registro
+   * @return integer
+   */
+  public function getDeletedAt() {
+    return $this->deletedAt;
+  }
+
+  /**
+   * Fija la configuración del sistema
+   * @version 1.0.0
+   * @param config $config Objeto con configuración del sistema
+   */
+  public function setConfig(config $config) {
+    $this->config = $config;
+  }
+
+  /**
+   * Fija el id para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $id
+   */
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  /**
+   * Fija el item para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $item
+   */
+  public function setItem($item) {
+    $this->item = $item;
+  }
+
+  /**
+   * Fija la cantidad de surco para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $cantidadSurco
+   */
+  public function setCantidadSurco($cantidadSurco) {
+    $this->cantidadSurco = $cantidadSurco;
+  }
+
+  /**
+   * Fija el numero de documento para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $numDocumento
+   */
+  public function setNumDocumento($numDocumento) {
+    $this->numDocumento = $numDocumento;
+  }
+
+  /**
+   * Fija la creacion de un nuevo registro en la tabla
+   * @version 1.0.0
+   * @param integer $createdAt
+   */
+  public function setCreatedAt($createdAt) {
+    $this->createdAt = $createdAt;
+  }
+
+  /**
+   * Fija la actualizacion de un registro en la tabla
+   * @version 1.0.0
+   * @param integer $updatedAt
+   */
+  public function setUpdatedAt($updatedAt) {
+    $this->updatedAt = $updatedAt;
+  }
+
+  /**
+   * Fija la eliminacion de un registro en la tabla
+   * @version 1.0.0
+   * @param integer $deletedAt
+   */
+  public function setDeletedAt($deletedAt) {
+    $this->deletedAt = $deletedAt;
+  }
+
+}
