f47b54cd0e8cd9452d172a0fae91ff645b3bd686
xubuntu <xubuntu@xubuntu-VirtualBox>
2015-11-24 08:48:23 -0500

    Modificacion de las tablas registro tractor y turno

---
+++ model/base/TurnoBaseTable.class.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace FStudio\model\base;
+
+use FStudio\fsModel as model;
+use FStudio\myConfig as config;
+
+/**
+ * Description of registroTractorBaseTable
+ * @author yuri adriana hurtado rojas <yurodri.1996@gmail.com>
+ * @package 
+ * @subpackage model
+ * @subpackage base
+ * @version 1.0.0
+ */
+class TurnoBaseTable extends model {
+
+  /**
+   * ID de la tabla
+   */
+  const ID = 'tur_id';
+
+  /**
+   * descripcion de la tabla turno
+   */
+  const DESCRIPCION = 'tur_descripcion';
+
+  /**
+
+   * hora de de inicio de la tabla turno
+   */
+  const Hora_Inicio = 'tur_hora_inicio';
+
+  /**
+   * Hora fin fe la tabla turno
+   */
+  const Hora_Fin = 'tur_hora_fin';
+
+  /**
+
+   * turno de la tabla estado
+   */
+  const ESTADO = 'tur_estado';
+
+  /**
+
+   * longitud de las observaciones
+   */
+  const TABLE = 'bda_Turno';
+
+  /**
+   * Configuración del sistema
+   * @var config
+   */
+  protected $config;
+
+  /**
+   * ID de la tabla
+   * @var integer
+   */
+  private $id;
+
+  /**
+   * Descripcion de la tabla
+   * @var varchar
+   */
+  private $descripcion;
+
+  /**
+   * hora inicio de la tabla
+   * @var time
+   */
+  private $hora_inicio;
+
+  /**
+   * hora fin de la tabla
+   * @var time
+   */
+  private $hora_fin;
+
+  /**
+   * estado de la tabla
+   * @var varchar
+   */
+  private $estado;
+  private $tabla;
+
+  /**
+   * crea la tabla
+   * @var time_stamp
+   */
+  private $createdAt;
+
+  /**
+   * actualiza la tabla
+   * @var time_stamp
+   */
+  private $updatedAt;
+
+  /**
+   * elimina la tabla
+   * @var time_stamp
+   */
+  private $deletedAt;
+
+  /**
+   * 
+   * @param config $config
+   * @param type $id
+   * @param varchar $descripcion
+   * @param time $hora_inicio
+   * @param time $hora_fin
+   * @param varchar $estado
+   * @param type $tabla
+   * @param time_stamp $createdAt
+   * @param time_stamp $updatedAt
+   * @param time_stamp $deletedAt
+   */
+  public function __construct(config $config, $id, varchar $descripcion, time $hora_inicio, time $hora_fin, varchar $estado, $tabla, time_stamp $createdAt, time_stamp $updatedAt, time_stamp $deletedAt) {
+    $this->config = $config;
+    $this->id = $id;
+    $this->descripcion = $descripcion;
+    $this->hora_inicio = $hora_inicio;
+    $this->hora_fin = $hora_fin;
+    $this->estado = $estado;
+    $this->tabla = $tabla;
+    $this->createdAt = $createdAt;
+    $this->updatedAt = $updatedAt;
+    $this->deletedAt = $deletedAt;
+  }
+
+  /**
+   * Retorna la configuración del sistema
+   * @version 1.0.0
+   * @return config
+   */
+  public function getConfig() {
+    return $this->config;
+  }
+
+  /**
+   * Retorna el id del registro
+   * version 1.0.o
+   * @return integer
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * Retorna la descripcion del registro
+   * version 1.0.o
+   * @return time_date
+   */
+  public function getDescripcion() {
+    return $this->descripcion;
+  }
+
+  /**
+   * Retorna la hora-inicio del registro
+   * version 1.0.o
+   * @return time_date
+   */
+  public function getHora_inicio() {
+    return $this->hora_inicio;
+  }
+
+  /**
+   * Retorna la hora_fin del registro
+   * version 1.0.o
+   * @return time_date
+   */
+  public function getHora_fin() {
+    return $this->hora_fin;
+  }
+
+  /**
+   * Retorna el estado del registro
+   * version 1.0.o
+   * @return varchar
+   */
+  public function getEstado() {
+    return $this->estado;
+  }
+
+  /**
+   * Retorna la tabla del registro
+   * version 1.0.o
+   * @return integer
+   */
+  public function getTabla() {
+    return $this->tabla;
+  }
+
+  /**
+   * Retorna los datos creados en la tabla
+   * @version 1.0.0
+   * @return time_stamp
+   */
+  public function getCreatedAt() {
+    return $this->createdAt;
+  }
+
+  /**
+   * Retorna las actualizaciones que se hayan hecho en la tabla
+   * @version 1.0.0
+   * @return time_stamp
+   */
+  public function getUpdatedAt() {
+    return $this->updatedAt;
+  }
+
+  /**
+   * Retorna los datos eliminados de la tabla
+   * @version 1.0.0
+   * @return time_stamp
+   */
+  public function getDeletedAt() {
+    return $this->deletedAt;
+  }
+
+  /**
+   * Fija la configuración del sistema
+   * @version 1.0.0
+   * @param config $config Objeto con configuración del sistema
+   */
+  public function setConfig(config $config) {
+    $this->config = $config;
+  }
+
+  /**
+   * Fija el id para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $id
+   */
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+/**
+   * Fija la descripcion para el registro en la tabla
+   * @version 1.0.0
+   * @param varchar $descripcion
+   */
+
+  public function setDescripcion(varchar $descripcion) {
+    $this->descripcion = $descripcion;
+  }
+
+  /**
+   * Fija la Hora_inicio para el registro en la tabla
+   * @version 1.0.0
+   * @param date_time $Hora_inicio
+   */
+  public function setHora_inicio(time $hora_inicio) {
+    $this->hora_inicio = $hora_inicio;
+  }
+
+  /**
+   * Fija la Hora_fin para el registro en la tabla
+   * @version 1.0.0
+   * @param date_time $Hora_fin
+   */
+  public function setHora_fin(time $hora_fin) {
+    $this->hora_fin = $hora_fin;
+  }
+
+  /**
+   * Fija el estado para el registro en la tabla
+   * @version 1.0.0
+   * @param varchar $estado
+   */
+  public function setEstado(varchar $estado) {
+    $this->estado = $estado;
+  }
+
+  /**
+   * Fija el registro en la tabla
+   * @version 1.0.0
+   * @param integer $tabla
+   */
+  public function setTabla($tabla) {
+    $this->tabla = $tabla;
+  }
+
+  /**
+   * Fija los datos creados en la tabla
+   * @version 1.0.0
+   * @return time_stamp
+   */
+  public function setCreatedAt(time_stamp $createdAt) {
+    $this->createdAt = $createdAt;
+  }
+
+  /**
+   * Fija las actualizaciones que se hayan hecho en la tabla
+   * @version 1.0.0
+   * @return time_stamp
+   */
+  public function setUpdatedAt(time_stamp $updatedAt) {
+    $this->updatedAt = $updatedAt;
+  }
+
+  /**
+   * Fijalos datos eliminados de la tabla
+   * @version 1.0.0
+   * @return time_stamp
+   */
+  public function setDeletedAt(time_stamp $deletedAt) {
+    $this->deletedAt = $deletedAt;
+  }
+
+}
---
+++ model/registroTractorTable.class.php
@@ -0,0 +1,99 @@
+<?php
+
+use FStudio\model\base\productoBaseTable;
+
+/**
+ * Description of registroTractorTable
+ * @author yuri adriana hurtado rojas <yurodri.1996@gmail.com>
+ * @package
+ * @subpackage model
+ * @subpackage table
+ * @version 1.0.0
+ */
+class registroTractor extends registroTractorBaseTable {
+  /**
+   * Obtiene todos los datos de la tabla
+   * @return [stdClass | boolean]
+   */
+
+  public function getAll() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'SELECT rt_item, csc_id, rt_horaSalida, rt_tractor, rt_observacion, rt_tabla, rt_created_at, rt_update_at, rt_deleted_at FROM registroTractor ORDER BY rt_created_at, rt_updated_at, rt_deletyed_at ASC';
+    $answer = $conn->prepare($sql);
+    $answer->execute();
+    return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
+  }
+   /**
+   * Retorna un elemento de la tabla buscado por un id especifico
+   * @param integer $id
+   * @return [stdClass | boolean]
+   */
+
+  public function getById($item) {
+    $conn = $this->getConnection($this->config);
+    $sql = 'SELECT rt_item, csc_id, rt_horaSalida, rt_tractor, rt_observacion, rt_tabla, rt_created_at,rt_ updated_at, rt_deleted_at '
+            . 'FROM registroTractor '
+            . 'AND rt_item = :rt_item';
+    $params = array(
+        ':rt_item' => $item
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
+  }
+   /**
+   * Registra los datos del objeto en la tabla
+   * @return integer
+   */
+
+  public function save() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'INSERT INTO registroTractor (rt_horaSalida, rt_tractor, rt_observacion, rt_tabla ) VALUES (:rt_horaSalida, :rt_tractor, :rt_observacion, :rt_tabla, )';
+    $params = array(
+        ':rt_horaSalida' => $this->gethoraSalida(),
+        ':rt_tractor' => $this->gettractor(),
+        ':rt_observacion' => $this->getObservacion(),
+        ':tabla' => $this->getTabla(),
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return $conn->lastInsertId(self::_SEQUENCE);
+  }
+   /**
+   * Actualiza un registro de la tabla
+   * @return boolean
+   */
+
+  public function update() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'UPDATE registroTractor SET rt_horaSalida = :rt_horaSalida, rt_tractor = :rt_tractor, rt_observacion = :rt_observacion, rt_tabla = :rt_tabla,  WHERE rt_item = :rt_item';
+    $params = array(
+        ':rt_horaSalisa' => $this->gethoraSalida(),
+        ':rt_tractor' => $this->gettractor(),
+        ':rt_observacion' => $this->getObservacion(),
+        ':tabla' => $this->getTabla(),
+        ':rt_item' => $this->getItem()
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return true;
+  }
+   /**
+   * Borra en forma logica o fisica un registro de la tabla
+   * @param boolean $deleteLogical
+   * @return boolean
+   * @throws PDOException
+   */
+
+  public function delete() {
+    $conn = $this->getConnection($this->config);
+    $sql = 'DELETE FROM registroTractor WHERE rt_item = :rt_item';
+    $params = array(
+        ':rt_item' => $this->getIem()
+    );
+    $answer = $conn->prepare($sql);
+    $answer->execute($params);
+    return true;
+  }
+
+}
