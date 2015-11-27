e37bd0869db118cda2823ccdd02b49d1f69f181c
xubuntu <xubuntu@xubuntu-VirtualBox>
2015-11-27 09:17:40 -0500

    modificacion de las tablas

--- model/controlRiegoPluviosidadTable.Class.php
+++ model/controlRiegoPluviosidadTable.Class.php
@@ -8,58 +8,59 @@
  * @subpackage table
  * @version 1.0.0
  */
-class controlRiegoPluviosidadTable extends controlRiegoPluviosidadBaseTable {
-   /**
+class controlRiegoPluviosidadTable extends controlRiegoPluviosidadTableBase {
+
+  /**
    * Obtiene todos los datos de la tabla
    * @return [stdClass | boolean]
    */
-  
- public function getAll() {
+  public function getAll() {
     $conn = $this->getConnection($this->config);
     $sql = 'SELECT crp_id, crp_fecha, crp_hora_inicio, crp_hora_fin, crp_cantidad_m3_hora, crp_observacion, sue_id,hac_id, ter_id, crp_created_at, crp_updated_at, crp_deleted_at FROM controlRiegoPluviosidad WHERE deleted_at IS NULL ORDER BY created_at ASC';
     $answer = $conn->prepare($sql);
     $answer->execute();
     return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
   }
-   /**
+
+  /**
    * Retorna un elemento de la tabla buscado por un id especifico
    * @param integer $id
    * @return [stdClass | boolean]
    */
-public function getById($id) {
+  public function getById($id = null) {
     $conn = $this->getConnection($this->config);
     $sql = 'SELECT crp_id, crp_fecha, crp_hora_inicio, crp_hora_fin, crp_cantidad_m3_hora, crp_observacion, sue_id, hac_id, ter_id FROM bitacora AND id = :id';
     $params = array(
-        ':id' => $id
+        ':id' => ($id !== null) ? $id : $this->getId()
     );
     $answer = $conn->prepare($sql);
     $answer->execute($params);
     return ($answer->rowCount() > 0) ? $answer->fetchAll(PDO::FETCH_OBJ) : false;
   }
+
   /**
    * Registra los datos del objeto en la tabla
    * @return integer
    */
-  
-    public function save() {
+  public function save() {
     $conn = $this->getConnection($this->config);
     $sql = 'INSERT INTO controlRiegoPLuviosidad (fecha, hora_inicio, hora_fin, cantidad_m3_hora, observacion, sue_id, hac_id, ter_id) VALUES (:fecha, :hora_inicio, :hora_fin, :cantidad_m3_hora, :observacion, :sue_id, :hac_id, :ter_id)';
     $params = array(
         ':fecha' => $this->get(),
         ':hora_inicio' => $this->gethora_inicio(),
-        ':hora_fin' => $this->gethora_fin(),
+        ':hora_fin' => $this->gethoraFin(),
         ':cantidad_m3_hora' => $this->getcantidad_m3_hora(),
         ':observacion' => $this->getobservacion(),
-           ':sue_id' => $this->getsue_id(),
-         ':hac_id' => $this->gethac_id(),
-        ':ter_id' => $this->getter_id(),
+        ':sue_id' => $this->getsueId(),
+        ':hac_id' => $this->gethacId(),
+        ':ter_id' => $this->getterId(),
     );
     $answer = $conn->prepare($sql);
     $answer->execute($params);
-    return $conn->lastInsertId(self::_SEQUENCE);
+    $this -> setId($conn->lastInsertId(self::_SEQUENCE));
+    return true;
   }
-  
-  
+
   /**
    * Actualiza un registro de la tabla
    * @return boolean
@@ -69,25 +70,26 @@ public function getById($id) {
     $sql = 'UPDATE controlRiegoPluviosidad SET fecha = :fecha, hora_inicio = :hora_inicio, hora_fin = :hora_fin, cantidad_m3_hora = :cantidad_m3_hora, observacion = :observacion, sue_id :sue_id, hac_id :hac_id, :ter_id, ter_id WHERE id = :id';
     $params = array(
         ':fecha' => $this->getfecha(),
-        ':hora_inicio' => $this->gethora_inicio(),
-        ':hora_fin' => $this->gethora_fin(),
-        ':cantidad_m3_hora' => $this->getcantidad_m3_hora(),
+        ':hora_inicio' => $this->gethoraInicio(),
+        ':hora_fin' => $this->gethoraFin(),
+        ':cantidad_m3_hora' => $this->getcantidadM3Hora(),
         ':observacion' => $this->getobservacion(),
-        ':sue_id' => $this->getsue_id(),
-         ':hac_id' => $this->gethac_id(),
-         ':ter_id' => $this->getter_id()
+        ':sue_id' => $this->getsueId(),
+        ':hac_id' => $this->gethacId(),
+        ':ter_id' => $this->getterId()
     );
     $answer = $conn->prepare($sql);
     $answer->execute($params);
     return true;
   }
+
   /**
    * Borra en forma logica o fisica un registro de la tabla
    * @param boolean $deleteLogical
    * @return boolean
    * @throws PDOException
    */
-    public function delete() {
+  public function delete() {
     $conn = $this->getConnection($this->config);
     $sql = 'DELETE FROM controlRiegoPluviosidad WHERE id = :id';
     $params = array(
@@ -97,4 +99,5 @@ public function getById($id) {
     $answer->execute($params);
     return true;
   }
+
 }
