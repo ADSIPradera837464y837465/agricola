6aa9ce09cdbae78013f5135de2a21635f57558f5
xubuntu <xubuntu@xubuntu-VirtualBox>
2015-11-27 11:42:15 -0500

    Modificacion Tabla

--- model/base/caudalSurcoTableBase.class.php
+++ model/base/caudalSurcoTableBase.class.php
@@ -15,259 +15,266 @@ use FStudio\myConfig as config;
  */
 class caudalSurcoTableBase extends model {
 
-    /**
-     * ID de la tabla
-     * 
-     */
-    const ID = 'decs_id';
+  /**
+   * ID de la tabla
+   * 
+   */
+  const ID = 'decs_id';
 
-    /**
-     * Secuencia del identificador de la tabla
-     */
-    const ITEM = 'decs_item';
+  /**
+   * Secuencia del identificador de la tabla
+   */
+  const ITEM = 'decs_item';
 
-    /**
-     * Longitud del campo item
-     */
-    const ITEM_LENGTH = 80;
+  /**
+   * Longitud del campo item
+   */
+  const ITEM_LENGTH = 80;
 
-    /**
-     * cantidad de surco
-     */
-    const CANTIDAD_SURCO = 'decs_cantidad_surco';
+  /**
+   * cantidad de surco
+   */
+  const CANTIDAD_SURCO = 'decs_cantidad_surco';
 
-    /**
-     * numero de documento
-     */
-    const NUM_DOCUMENTO = 'fore_num_documento';
+  /**
+   * numero de documento
+   */
+  const NUM_DOCUMENTO = 'fore_num_documento';
 
-    /**
-     * Fecha y hora de creación del registro
-     */
-    const CREATED_AT = 'decs_created_at';
+  /**
+   * Fecha y hora de creación del registro
+   */
+  const CREATED_AT = 'decs_created_at';
 
-    /**
-     * Fecha y hora de la ultima actualización del registro
-     */
-    const UPDATED_AT = 'decs_updated_at';
+  /**
+   * Fecha y hora de la ultima actualización del registro
+   */
+  const UPDATED_AT = 'decs_updated_at';
 
-    /**
-     * Fecha y hora para controlar el borrado lógico
-     */
-    const DELETED_AT = 'decs_deleted_at';
+  /**
+   * Fecha y hora para controlar el borrado lógico
+   */
+  const DELETED_AT = 'decs_deleted_at';
 
-    /**
+  /**
    * Nombre de la secuencia del ID de la tabla
    */
   const _SEQUENCE = 'bda_detalle_caudal_surco_decs_id_seq';
 
-    /**
-     *  Nombre de la tabla 
-     */
-    const _TABLE = 'bda_detalle_caudal_surco';
+  /**
+   *  Nombre de la tabla 
+   */
+  const _TABLE = 'bda_detalle_caudal_surco';
 
-    /**
-     * Configuración del sistema
-     * @var config
-     */
-    protected $config;
+  /**
+   * Configuración del sistema
+   * @var config
+   */
+  protected $config;
 
-    /**
-     * ID de la tabla
-     * @var integer
-     */
-    private $id;
+  /**
+   * ID de la tabla
+   * @var integer
+   */
+  private $id;
 
-    /**
-     * Secuencia del identificador de la tabla
-     * @var string
-     */
-    private $item;
+  /**
+   * Secuencia del identificador de la tabla
+   * @var string
+   */
+  private $item;
 
-    /**
-     * cantidad de surco
-     * @var integer
-     */
-    private $cantidadSurco;
+  /**
+   * cantidad de surco
+   * @var integer
+   */
+  private $cantidadSurco;
 
-    /**
-     * numero de documento
-     * @var integer
-     */
-    private $numDocumento;
+  /**
+   * numero de documento
+   * @var integer
+   */
+  private $numDocumento;
 
-    /**
-     * fecha y hora de cuando se creo  un registro
-     * @var date_time
-     */
-    private $createdAt;
+  /**
+   * fecha y hora de cuando se creo  un registro
+   * @var date_time
+   */
+  private $createdAt;
 
-    /**
-     * fecha y hora de la ultima modificacion de un registro
-     * @var date_time
-     */
-    private $updatedAt;
-    
+  /**
+   * fecha y hora de la ultima modificacion de un registro
+   * @var date_time
+   */
+  private $updatedAt;
 
-    /**
-     * fecha y hora para controlar el borrado logico
-     * @var date_time
-     */
-    private $deletedAt;
-    public function __construct(config $config, $id, $item, $cantidadSurco, $numDocumento, date_time $createdAt, date_time $updatedAt, date_time $deletedAt) {
-        $this->config = $config;
-        $this->id = $id;
-        $this->item = $item;
-        $this->cantidadSurco = $cantidadSurco;
-        $this->numeroDocumento = $numDocumento;
-        $this->createdAt = $createdAt;
-        $this->updatedAt = $updatedAt;
-        $this->deletedAt = $deletedAt;
-    }
+  /**
+   * Constructor de la clase caudalSurcoTableBase
+   * @version 1.0.0
+   * @param config $config
+   * @param integer $id
+   * @param integer $item
+   * @param integer $cantidadSurco
+   * @param integer $numDocumento
+   * @param date_time $createdAt
+   * @param date_time $updatedAt
+   * @param date_time $deletedAt
+   */
+  private $deletedAt;
 
-    
-    /**
-     * Retorna la configuración del sistema
-     * @version 1.0.0
-     * @return config
-     */
-    public function getConfig() {
-        return $this->config;
-    }
+  public function __construct(config $config, $id = null, $item = null, $cantidadSurco = null, $numDocumento = null, date_time $createdAt = null, date_time $updatedAt = null, date_time $deletedAt = null) {
+    $this->config = $config;
+    $this->id = $id;
+    $this->item = $item;
+    $this->cantidadSurco = $cantidadSurco;
+    $this->numeroDocumento = $numDocumento;
+    $this->createdAt = $createdAt;
+    $this->updatedAt = $updatedAt;
+    $this->deletedAt = $deletedAt;
+  }
 
-    /**
-     * Retorna el id del registro
-     * @version 1.0.0
-     * @return integer
-     */
-    public function getId() {
-        return $this->id;
-    }
+  /**
+   * Retorna la configuración del sistema
+   * @version 1.0.0
+   * @return config
+   */
+  public function getConfig() {
+    return $this->config;
+  }
 
-    /**
-     * Retorna la secuencia del registro
-     * @version 1.0.0
-     * @return string
-     */
-    public function getItem() {
-        return $this->item;
-    }
+  /**
+   * Retorna el id del registro
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getId() {
+    return $this->id;
+  }
 
-    /**
-     * Retorna la cantidad del registro
-     * @version 1.0.0
-     * @return integer
-     */
-    public function getCantidadSurco() {
-        return $this->cantidadSurco;
-    }
+  /**
+   * Retorna la secuencia del registro
+   * @version 1.0.0
+   * @return string
+   */
+  public function getItem() {
+    return $this->item;
+  }
 
-    /**
-     * Retorna el numero del documento
-     * @version 1.0.0
-     * @return integer
-     */
-    public function getNumeroDocumento() {
-        return $this->numDocumento;
-    }
+  /**
+   * Retorna la cantidad del registro
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getCantidadSurco() {
+    return $this->cantidadSurco;
+  }
 
-    /**
-     * Retorna fecha y hora de cuando se creo un registro
-     * @version 1.0.0
-     * @return integer
-     */
-    public function getCreatedAt() {
-        return $this->createdAt;
-    }
+  /**
+   * Retorna el numero del documento
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getNumDocumento() {
+    return $this->numDocumento;
+  }
 
-    /**
-     * Retorna fecha y hora de cuando se modifico el ultimo registro
-     * @version 1.0.0
-     * @return integer
-     */
-    public function getUpdatedAt() {
-        return $this->updatedAt;
-    }
+  /**
+   * Retorna fecha y hora de cuando se creo un registro
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getCreatedAt() {
+    return $this->createdAt;
+  }
 
-    /**
-     * Retorna fecha y hora el borrado logico
-     * @version 1.0.0
-     * @return integer
-     */
-    public function getDeletedAt() {
-        return $this->deletedAt;
-    }
+  /**
+   * Retorna fecha y hora de cuando se modifico el ultimo registro
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getUpdatedAt() {
+    return $this->updatedAt;
+  }
 
-    /**
-     * Fija la configuración del sistema
-     * @version 1.0.0
-     * @param config $config Objeto con configuración del sistema
-     */
-    public function setConfig(config $config) {
-        $this->config = $config;
-    }
+  /**
+   * Retorna fecha y hora el borrado logico
+   * @version 1.0.0
+   * @return integer
+   */
+  public function getDeletedAt() {
+    return $this->deletedAt;
+  }
 
-    /**
-     * Fija el id para el registro en la tabla
-     * @version 1.0.0
-     * @param integer $id
-     */
-    public function setId($id) {
-        $this->id = $id;
-    }
+  /**
+   * Fija la configuración del sistema
+   * @version 1.0.0
+   * @param config $config Objeto con configuración del sistema
+   */
+  public function setConfig(config $config) {
+    $this->config = $config;
+  }
 
-    /**
-     * Fija la secuencia para el registro en la tabla
-     * @version 1.0.0
-     * @param integer $item
-     */
-    public function setItem($item) {
-        $this->item = $item;
-    }
+  /**
+   * Fija el id para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $id
+   */
+  public function setId($id) {
+    $this->id = $id;
+  }
 
-    /**
-     * Fija la cantidad para el registro en la tabla
-     * @version 1.0.0
-     * @param integer $cantidadSurco
-     */
-    public function setCantidadSurco($cantidadSurco) {
-        $this->cantidadSurco = $cantidadSurco;
-    }
+  /**
+   * Fija la secuencia para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $item
+   */
+  public function setItem($item) {
+    $this->item = $item;
+  }
 
-    /**
-     * Fija el numero de documento para el registro en la tabla
-     * @version 1.0.0
-     * @param integer $numDocumento
-     */
-    public function setNumDocumento($numDocumento) {
-        $this->numeroDocumento = $numeroDocumento;
-    }
+  /**
+   * Fija la cantidad para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $cantidadSurco
+   */
+  public function setCantidadSurco($cantidadSurco) {
+    $this->cantidadSurco = $cantidadSurco;
+  }
 
-    /**
-     * Fija fecha y hora de cuando se creo  un registro
-     * @version 1.0.0
-     * @param integer $createdAt
-     */
-    public function setCreatedAt(date_time $createdAt) {
-        $this->createdAt = $createdAt;
-    }
+  /**
+   * Fija el numero de documento para el registro en la tabla
+   * @version 1.0.0
+   * @param integer $numDocumento
+   */
+  public function setNumDocumento($numDocumento) {
+    $this->numDocumento = $numDocumento;
+  }
 
-    /**
-     * Fija fecha y hora de cuando se modifico el ultimo registro
-     * @version 1.0.0
-     * @param integer $updatedAt
-     */
-    public function setUpdatedAt(date_time $updatedAt) {
-        $this->updatedAt = $updatedAt;
-    }
+  /**
+   * Fija fecha y hora de cuando se creo  un registro
+   * @version 1.0.0
+   * @param integer $createdAt
+   */
+  public function setCreatedAt(date_time $createdAt) {
+    $this->createdAt = $createdAt;
+  }
 
-    /**
-     * Fija fecha y hora del borrado logico
-     * @version 1.0.0
-     * @param integer $deletedAt
-     */
-    public function setDeletedAt(date_time $deletedAt) {
-        $this->deletedAt = $deletedAt;
-    }
+  /**
+   * Fija fecha y hora de cuando se modifico el ultimo registro
+   * @version 1.0.0
+   * @param integer $updatedAt
+   */
+  public function setUpdatedAt(date_time $updatedAt) {
+    $this->updatedAt = $updatedAt;
+  }
+
+  /**
+   * Fija fecha y hora del borrado logico
+   * @version 1.0.0
+   * @param integer $deletedAt
+   */
+  public function setDeletedAt(date_time $deletedAt) {
+    $this->deletedAt = $deletedAt;
+  }
 
 }
