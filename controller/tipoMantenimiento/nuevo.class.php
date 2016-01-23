45f10e40dfb978f5c3cf89eba6098e20e5c03529
Nani <Nani@192.168.100.2>
2016-01-23 13:44:48 -0500

    crud tablas orden servicio y tipo mantenimiento

--- controller/tipoMantenimiento/nuevo.class.php
+++ controller/tipoMantenimiento/nuevo.class.php
@@ -4,14 +4,14 @@ use FStudio\fsController as controller;
 use FStudio\interfaces\fsAction as action;
 
 /**
- * Description nuevo
+ * Description of nuevo
  * @author Itiani Moreno Rosero <itiani2811@gmail.com>
  */
 class nuevo extends controller implements action {
 
   public function execute() {
 
-    $this->defineView('tipoMantenimiento', 'nuevo', 'html');
+    $this->defineView('tipoMantenimiento', 'nuevo', 'html');  
   }
 
 }
