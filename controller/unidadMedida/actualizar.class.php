36e8acbd55da1becfdca9900ea6600900fc5785e
equipo1 <equipo1@Lenovo-PC1.home>
2016-01-29 22:24:34 -0500

    CRUD Tablas unidad_medida y tipo_producto

--- controller/unidadMedida/actualizar.class.php
+++ controller/unidadMedida/actualizar.class.php
@@ -1,16 +1,32 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once '../model/base/unidadMedidaBaseTable.class.php';
+require_once '../model/unidadMedidaTable.class.php';
+
+use FStudio\fsController as controller;
+use FStudio\interfaces\fsAction as action;
 
 /**
  * Description of actualizar
  *
- * @author xubuntu
+ * @author Angela Cardona <angela04cardona@hotmail.com>
+ * @package FStudio
+ * @subpackage controller
+ * @subpackage index.class.php
+ * @version 1.0.0
  */
-class actualizar {
-  //put your code here
+class actualizar extends controller implements action {
+
+  public function execute() {
+    $formUnidadMedida = filter_input_array(INPUT_POST)['unidadMedida'];
+
+    $unidadMedida = new unidadMedidaTable();
+    $unidadMedida->setId($formUnidadMedida['unmId']);
+    $unidadMedida->setDescripcion($formUnidadMedida['unmDes']);
+    $this->objUnidadMedida = $unidadMedida->update();
+
+
+    header('Location: ' . $fsConfig->getUrl() . 'index.php/unidadMedida/index');
+  }
+
 }
