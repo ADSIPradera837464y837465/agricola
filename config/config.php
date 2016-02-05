e9d346ccf719ee34a9ed37e079430d72d876e346
Duvier <duvier_24@hotmail.es>
2016-02-05 14:19:06 -0500

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+use FStudio\myConfig as config;
+
+$config = new config();
+
+$config->setPath('/var/www/html/agricola/');
+$config->setUrl('http://localhost/agricola/web/');
+
+$config->setDriver('pgsql');
+$config->setHost('localhost');
+$config->setPort(5432);
+$config->setDbName('agro');
+$config->setUser('postgres');
+$config->setPassword('123');
+$config->setDsn($config->getDriver() . ':host=' . $config->getHost() . ';port=' . $config->getPort() . ';dbname=' . $config->getDbName());
+
+$config->setSessionName('FStudio');
+
+$config->setDefaultModule('ejemplo');
+$config->setDefaultAction('index');
